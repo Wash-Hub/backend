5e6e6151fbba5b684713fbed0eb942e58dd9cdf5
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-07-12 22:01:25 +0900

    feat: main.ts swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { BaseAPIDocument } from './common/config/swagger.documemt';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +16,10 @@ async function bootstrap() {
   });
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalPipes(new ValidationPipe({ transform: true })); //적용을하겠다
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); //적용을하겠다\
+  const config = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(8000);
 }
 bootstrap();
