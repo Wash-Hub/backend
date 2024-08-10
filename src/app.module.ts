27275d10450061f422c55cce7a7596241607f712
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-08-10 12:29:56 +0900

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,20 @@ import { AppConfigModule } from './common/config/config.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { MapReviewModule } from './map-review/map-review.module';
+import { AwsModule } from './aws/aws.module';
 import { MapModule } from './map/map.module';
 
 @Module({
-  imports: [AppConfigModule, UserModule, DatabaseModule, AuthModule, MapModule],
+  imports: [
+    AppConfigModule,
+    UserModule,
+    DatabaseModule,
+    AuthModule,
+    MapModule,
+    MapReviewModule,
+    AwsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
