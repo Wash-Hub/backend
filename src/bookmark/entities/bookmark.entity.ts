94a0fc3b0e221b0832e0cb18f8e37ff8fda8a69e
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-09-11 19:53:03 +0900

    entity설정 및 관계설정

---
+++ src/bookmark/entities/bookmark.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { CommonEntity } from '../../common/entities/common.entity';
+import { User } from '../../user/entities/user.entity';
+import { Map } from '../../map/entities/map.entity';
+
+@Entity()
+export class Bookmark extends CommonEntity {
+  @ManyToOne(() => User, (user: User) => user.bookmark, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  public user: User;
+
+  @ManyToOne(() => Map, (map: Map) => map.bookmark, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  public map?: Map;
+}
