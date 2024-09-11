48971b07368682f97ae4db82a634eb8fee09a19a
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-09-11 19:53:40 +0900

    컨트롤

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Req,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { BookmarkService } from './bookmark.service';
+import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { UpdateBookmarkDto } from './dto/update-bookmark.dto';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAccessAuthGuard } from '../auth/guard/jwtAccess-auth.guard';
+import { RequestWithUserInterface } from '../auth/requestWithUser.interface';
+
+@ApiTags('Bookmark')
+@Controller('bookmark')
+export class BookmarkController {
+  constructor(private readonly bookmarkService: BookmarkService) {}
+
+  @Post('create')
+  @ApiBody({ type: CreateBookmarkDto })
+  @ApiOperation({
+    summary: '북마크표시',
+  })
+  @ApiOperation({
+    description: 'Bookmark',
+  })
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAccessAuthGuard)
+  async bookMarkMao(
+    @Req() req: RequestWithUserInterface,
+    @Body() createBookmarkDto: CreateBookmarkDto,
+  ) {
+    const bookMark = await this.bookmarkService.createBookMark(
+      req.user,
+      createBookmarkDto,
+    );
+    return bookMark;
+  }
+}
